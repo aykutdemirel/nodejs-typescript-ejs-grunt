7f6a33983ec3936d51890cf149f75173abe23149
Aykut Demirel <ayktdemirel@hotmail.com>
2016-07-24 03:22:29 +0300

    updated

--- views/components/hotspots/spots.js
+++ views/components/hotspots/spots.js
@@ -1,3 +1,28 @@
+function getOffset(elem) {
+    var top=0, left=0;
+    while(elem) {
+        top = top + parseInt(elem.offsetTop);
+        left = left + parseInt(elem.offsetLeft);
+        elem = elem.offsetParent;
+    }
+    return {top: top, left: left};
+}
+
+var MiniPopup = React.createClass({
+    componentDidMount: function() {
+        this.refs.minipopup.style.top = (this.props.position.top - (this.refs.minipopup.offsetHeight / 2) + 25) + "px";
+        this.refs.minipopup.style.left = (this.props.position.left - this.refs.minipopup.offsetWidth) + "px";
+        this.refs.minipopup.setAttribute("class","minipopup opacity-1");
+    },
+    render:function () {
+        return(
+            <span ref="minipopup" id="minipopup" className="minipopup">
+                <h4>{this.props.title}</h4>
+                <p>{this.props.body}</p>
+            </span>
+        )
+    }
+});
 
 var SpotButton = React.createClass({
     getInitialState: function() {
@@ -6,30 +31,26 @@ var SpotButton = React.createClass({
         }
     },
     handleClick:function () {
-        alert('test');
-        this.setState( { condition : !this.state.condition } );
+        var position = getOffset(this.refs.spotButton);
+        this.setState({
+            condition: !this.state.condition
+        });
+        var cloneNode = document.getElementById("minipopup");
+        if(cloneNode && cloneNode!==null){
+            document.getElementById("minipopupWrapper").removeChild(cloneNode);
+        }else{
+            ReactDOM.render(<MiniPopup title={this.props.title} body={this.props.body} position={position} />,document.getElementById("minipopupWrapper"));
+        }
+
     },
     render: function () {
         return (
-            <span className={this.state.condition ? "plus-btn-wrapper on" :"plus-btn-wrapper"} data-x={this.props.position[0]} data-y={this.props.position[1]} >
-                <span className="plus-btn" onClick={this.handleClick}>
+            <span ref="spotButton" onClick={this.handleClick} className={this.state.condition ? "plus-btn-wrapper on" :"plus-btn-wrapper"} data-x={this.props.position[0]} data-y={this.props.position[1]} >
+                <span className="plus-btn">
                     <span className="glyphicon glyphicon-plus"></span>
                 </span>
             </span>
         )
-    }
-});
-
-var LargeSpot = React.createClass({
-    render: function() {
-        return (
-            <div className="col-xs-7 p-0 pl-9 col-2">
-                <div className="imgScl">
-                    <img src="assets/img/img-2.jpg" className="scale" data-scale="best-fill" data-align="bottom-right" />
-                    <SpotButton position={data.module.hotSpots.popups[1].config.coords} />
-                </div>
-            </div>
-        );
     }
 });
 
@@ -39,7 +60,20 @@ var SmallSpot = React.createClass({
             <div className="col-xs-5 p-0 pr-9 col-1">
                 <div className="imgScl">
                     <img src="assets/img/img-1.jpg" className="scale" data-scale="best-fill" data-align="top-left" />
-                    <SpotButton position={data.module.hotSpots.popups[0].config.coords} />
+                    <SpotButton position={data.module.hotSpots.popups[0].config.coords} title={data.module.hotSpots.popups[0].headline} body={data.module.hotSpots.popups[0].bodyCopy} />
+                </div>
+            </div>
+        );
+    }
+});
+
+var LargeSpot = React.createClass({
+    render: function() {
+        return (
+            <div className="col-xs-7 p-0 pl-9 col-2">
+                <div className="imgScl">
+                    <img src="assets/img/img-2.jpg" className="scale" data-scale="best-fill" data-align="bottom-right" />
+                    <SpotButton position={data.module.hotSpots.popups[1].config.coords} title={data.module.hotSpots.popups[1].headline} body={data.module.hotSpots.popups[1].bodyCopy} />
                 </div>
             </div>
         );
@@ -47,7 +81,6 @@ var SmallSpot = React.createClass({
 });
 
 require(['../assets/js/core/data.js'],function () {
-
     var Spots = React.createClass({
         componentDidMount: function() {
             require(['../assets/js/core/scaleImg'],function () {
@@ -64,7 +97,6 @@ require(['../assets/js/core/data.js'],function () {
             );
         }
     });
-
     ReactDOM.render(<Spots />,document.getElementById('hotspots'));
 });
 
