0335ae0e50aabaf3490a8aacba89efc3d82a6918
Aykut Demirel <ayktdemirel@hotmail.com>
2016-07-24 20:30:42 +0300

    updated

--- views/components/hotspots/spots.js
+++ views/components/hotspots/spots.js
@@ -1,8 +1,33 @@
+function getWindowSize() {
+
+    var size = {width:0,height:0};
+
+    if (self.innerWidth) {
+        size.width = self.innerWidth;
+    }
+    else if (document.documentElement && document.documentElement.clientHeight){
+        size.width = document.documentElement.clientWidth;
+    }
+    else if (document.body) {
+        size.width = document.body.clientWidth;
+    }
+
+    if (self.innerHeight) {
+        size.height = self.innerHeight;
+    }else if (document.documentElement && document.documentElement.clientHeight) {
+        size.height = document.documentElement.clientHeight;
+    }else if (document.body) {
+        size.height = document.body.clientHeight;
+    }
+
+    return size;
+
+}
 
 var MiniPopup = React.createClass({
     render:function () {
         return(
-            <span ref="minipopup" id="minipopup" className={this.props.visibility ? "minipopup active opacity-1 "+this.props.popupPosition :"minipopup "+this.props.popupPosition}>
+            <span ref="minipopup" id="minipopup" className={this.props.visibility ? "minipopup active opacity-1 "+this.props.popupPosition + " " + this.props.mobile :"minipopup "+this.props.popupPosition + " "+ this.props.mobile}>
                 <h4>{this.props.title}</h4>
                 <p>{this.props.body}</p>
             </span>
@@ -13,13 +38,48 @@ var MiniPopup = React.createClass({
 var SpotButton = React.createClass({
     getInitialState: function() {
         return {
-            condition: false
+            condition: false,
+            isMobile: false
         }
     },
     handleClick:function () {
-        this.setState({
-            condition: !this.state.condition
-        });
+
+        var windowSize = getWindowSize();
+
+        if(windowSize.width<767){
+
+            this.setState({
+                condition: !this.state.condition,
+                isMobile: true
+            });
+
+            if(!this.state.condition){
+                document.body.style.overflow = "hidden";
+            }else{
+                document.body.style.overflow = "auto";
+            }
+
+            this.refs.minitemplate.refs.minipopup.style.height = windowSize.height + "px";
+
+        }else{
+
+            if(!this.state.condition){
+                document.body.style.overflow = "hidden";
+            }else{
+                document.body.style.overflow = "auto";
+            }
+
+            document.body.style.overflow = "auto";
+
+            this.refs.minitemplate.refs.minipopup.style.height = "auto";
+
+            this.setState({
+                condition: !this.state.condition,
+                isMobile: false
+            });
+
+        }
+
         this.refs.minitemplate.refs.minipopup.style.top = (((this.refs.minitemplate.refs.minipopup.offsetHeight*-1)/2)+25) + "px";
     },
     render: function () {
@@ -28,7 +88,7 @@ var SpotButton = React.createClass({
                 <span className="plus-btn">
                     <span className="glyphicon glyphicon-plus"></span>
                 </span>
-                <MiniPopup ref="minitemplate" popupPosition={this.props.popupPosition} title={this.props.title} body={this.props.body} visibility={this.state.condition} />
+                <MiniPopup ref="minitemplate" popupPosition={this.props.popupPosition} title={this.props.title} body={this.props.body} visibility={this.state.condition} mobile={this.state.isMobile?"mobile":""} />
             </span>
         )
     }
@@ -40,6 +100,10 @@ var SmallSpot = React.createClass({
             <div className="col-md-5 col-sm-5 col-xs-12 p-0 pr-9">
                 <div className="imgScl">
                     <img src="assets/img/img-1.jpg" className="scale" data-scale="best-fill" data-align="top-left" />
+                    <div className="back">
+                        <h4>{data.module.hotSpots.popups[0].headline}</h4>
+                        <p>{data.module.hotSpots.popups[0].bodyCopy}</p>
+                    </div>
                 </div>
                 <SpotButton popupPosition="right" position={data.module.hotSpots.popups[0].config.coords} title={data.module.hotSpots.popups[0].headline} body={data.module.hotSpots.popups[0].bodyCopy} />
             </div>
@@ -53,6 +117,10 @@ var LargeSpot = React.createClass({
             <div className="col-md-7 col-sm-7 col-xs-12 p-0 pl-9">
                 <div className="imgScl">
                     <img src="assets/img/img-2.jpg" className="scale" data-scale="best-fill" data-align="bottom-right" />
+                    <div className="back">
+                        <h4>{data.module.hotSpots.popups[1].headline}</h4>
+                        <p>{data.module.hotSpots.popups[1].bodyCopy}</p>
+                    </div>
                 </div>
                 <SpotButton popupPosition="left" position={data.module.hotSpots.popups[1].config.coords} title={data.module.hotSpots.popups[1].headline} body={data.module.hotSpots.popups[1].bodyCopy} />
             </div>
